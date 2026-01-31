5abb46fb7ae934fdf9481805a3928a3fca85eda6
Christopher Moore <christopher.e.moore@gmail.com>
2026-01-31 18:31:50 +0700

    more

--- env.lua
+++ env.lua
@@ -43,6 +43,7 @@ return function(env)
 
 	-- matrix
 
+	env.vec2x2f = require 'vec-ffi.vec2x2f'
 	env.vec3x3f = require 'vec-ffi.vec3x3f'
 	env.vec4x4f = require 'vec-ffi.vec4x4f'
 
---
+++ vec2x2f.lua
@@ -0,0 +1,2 @@
+require 'vec-ffi.vec2f'
+return require 'vec-ffi.create_vec2'{ctype='vec2f_t'}
